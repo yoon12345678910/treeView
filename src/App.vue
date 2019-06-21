8f05ee358c6878ab59dd2297ea115f473266bd0c
Jongik Yoon <yji1221@gmail.com>
2019-06-21 19:04:56 +0900

    zTree vue convert

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <tree-view :treeViewData="treeViewData"/> -->
+    <ztree 
+      :nodes="nodes" 
+      :settings="settings" 
+      :selectId="'luna'"  
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import HelloWorld from './components/HelloWorld.vue'
+  import ztree from './components/ztree.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld,
+      'ztree': ztree
+    },
+    data: function () {
+      return {
+        nodes: [
+          { id: 'luna', pId: 'root', name: "루나소프트", expanded: true, children: [
+            { id: 'main', name: '메인', isParent: true, expanded: true },
+            { id: 'admin', name: '관리자', isParent: true, expanded: true, children: [
+              { id: 'account', name: '계정관리' },
+              { id: 'auth', name: '권한관리' },
+              { id: 'menu', name: '메뉴관리' },
+            ]},
+            { id: 'member', name: '회원관리', isParent: true, expanded: true },
+            { id: 'business', name: '영업관리', isParent: true, expanded: true, children: [
+              { id: 'businessCheck', pId: 'business', name: '영업조회'},
+            ]},
+            { id: 'service', name: '서비스관리', isParent: true, expanded: true, children: [
+              { id: 'serviceSetting', name: '서비스세팅' }
+            ]}
+          ]}
+        ],
+        settings: {
+          callback: {
+            onDblClick: (event, treeId, treeNode) => {
+              console.log('parent')
+            },
+          }
+        }
+
+      }
+    },
+     methods:{
+
+    }
 }
 </script>
 
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    font-family: Menlo, Consolas, monospace;
+    color: #444;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vZtree from 'v-ztree';
+// import jQuery from 'jquery';
+
+// Vue.use(jQuery);
+Vue.use(vZtree);
 
 Vue.config.productionTip = false
 
