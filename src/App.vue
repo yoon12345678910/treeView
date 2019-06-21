07fde8cdae762396faca47e54e723fb71ce8b57e
Jongik Yoon <yji1221@gmail.com>
2019-06-22 02:28:17 +0900

    click, getSelectedNodes, moveNode

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,98 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <!-- <tree-view :treeViewData="treeViewData"/> -->
     <ztree 
-      :nodes="nodes" 
-      :settings="settings" 
-      :selectId="'luna'"  
+      :treeId="'tree-test-001'"
+      :nodes="ztreeNodes" 
+      :settings="ztreeSettings" 
+      :selectId="'luna'" 
     />
+    <div>
+      <input
+        disabled
+        v-model="selectedNodeName"
+        type="text"
+      />
+      <button 
+        @click="getSelectedNodes"
+        type="button">
+        선택된 항목 찾기
+      </button>
+    </div>
+    <div>
+      <input 
+        v-model="moveNodeText"
+        type="text" 
+        placeholder="선택할 항목 ID를 입력하세요" 
+      />
+      <button 
+        @click="moveNode"
+        type="button">
+        이동
+      </button>
+    </div>
   </div>
 </template>
 
 <script>
-  import HelloWorld from './components/HelloWorld.vue'
   import ztree from './components/ztree.vue'
 
   export default {
     name: 'app',
     components: {
-      HelloWorld,
       'ztree': ztree
     },
     data: function () {
       return {
-        nodes: [
-          { id: 'luna', pId: 'root', name: "루나소프트", expanded: true, children: [
-            { id: 'main', name: '메인', isParent: true, expanded: true },
-            { id: 'admin', name: '관리자', isParent: true, expanded: true, children: [
-              { id: 'account', name: '계정관리' },
-              { id: 'auth', name: '권한관리' },
-              { id: 'menu', name: '메뉴관리' },
+        moveNodeText: '',
+        selectedNodeName: '',
+        ztreeId: '',
+        ztreeObj: null,
+        ztreeNodes: [
+          { id: 'luna', pId: 'root', name: "ROOT / luna", expanded: true, children: [
+            { id: 'main', name: '메인 / main', isParent: true, expanded: true },
+            { id: 'admin', name: '관리자 / admin', isParent: true, expanded: true, children: [
+              { id: 'account', name: '계정관리 / account' },
+              { id: 'auth', name: '권한관리 / auth' },
+              { id: 'menu', name: '메뉴관리 / menu' },
             ]},
-            { id: 'member', name: '회원관리', isParent: true, expanded: true },
-            { id: 'business', name: '영업관리', isParent: true, expanded: true, children: [
+            { id: 'member', name: '회원관리 / member', isParent: true, expanded: true },
+            { id: 'business', name: '영업관리 / business', isParent: true, expanded: true, children: [
               { id: 'businessCheck', pId: 'business', name: '영업조회'},
             ]},
-            { id: 'service', name: '서비스관리', isParent: true, expanded: true, children: [
-              { id: 'serviceSetting', name: '서비스세팅' }
+            { id: 'service', name: '서비스관리 / service', isParent: true, expanded: true, children: [
+              { id: 'serviceSetting', name: '서비스세팅 / serviceSetting' }
             ]}
           ]}
         ],
-        settings: {
+        ztreeSettings: {
           callback: {
-            onDblClick: (event, treeId, treeNode) => {
-              console.log('parent')
+            completed: (ztreeId, ztreeObj) => {
+              console.log('completed: ', ztreeId, ztreeObj)
+              this.ztreeId = ztreeId;
+              this.ztreeObj = ztreeObj;
+              this.getSelectedNodes();
+            },
+            beforeClick: (treeId, treeNode) => {
+              console.log('beforeClick - isSelectedNode: ', $.fn.zTree.getZTreeObj(treeId).isSelectedNode(treeNode));
+            },
+            onClick: (event, treeId, treeNode) => {
+              console.log('onClick - isSelectedNode: ', $.fn.zTree.getZTreeObj(treeId).isSelectedNode(treeNode));
             },
           }
         }
-
-      }
+      };
     },
-     methods:{
-
+     methods: {
+      getSelectedNodes: function () {
+        console.log('getSelectedNodes: ', this.ztreeObj.getSelectedNodes());
+        this.selectedNodeName = this.ztreeObj.getSelectedNodes()[0].name;
+      },
+      moveNode: function () {
+        this.ztreeObj.selectNode(this.ztreeObj.getNodeByParam('id', this.moveNodeText))
+      }
     }
 }
 </script>
-
 
 <style>
   body {
--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import vZtree from 'v-ztree';
-// import jQuery from 'jquery';
-
-// Vue.use(jQuery);
-Vue.use(vZtree);
 
 Vue.config.productionTip = false
 
